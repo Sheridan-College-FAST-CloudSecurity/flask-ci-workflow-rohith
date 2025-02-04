79c502aebfe2d87aa54eee29380dcc9d2565c141
Rohith suri <surirohith77@gmail.com>
2025-02-04 21:37:35 +0000

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-       return "Hello, DevSecOps! modified after failure verified in settings actions as well"
+    return "Hello, DevSecOps! modified after failure verified in settings actions as well"
 
 if __name__ == "__main__":
-       app.run()
+    app.run()
